a72c720c1b29fac4f28268c0d755b823a10c7464
Vallabhkejgir <k_vshriniwas@ch.iitr.ac.in>
2025-06-18 05:45:05 +0530

    API and Error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 
 import dotenv from 'dotenv';
 
-import connectDB from "./db/index.js";
+import connectDB from "./db/db.js";
+import { app } from './app.js';
 
 dotenv.config({
     path: '\.env'
 })
 
-connectDB();
+connectDB()
+
+.then(() => {
+
+    app.on("error", (error) => {            // fsdjfnjsdn
+        console.log("ERRR : ", error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at port : ${process.env.PORT}`);
+    })
+})
+
+.catch((err) => {
+    console.log("MONGO db Connection Fail", err);
+})
